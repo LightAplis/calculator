c9e18c5f6d7c304d7ea3a28244fa233538379d4d
LightAplis <119054849+LightAplis@users.noreply.github.com>
2022-12-03 16:01:36 +0800

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -3,8 +3,10 @@
 using namespace std; 
 int main(){
 	cout<<"这是一个计算器，目前只支持两个数的运算。"<<endl<<"支持加、减、乘、除、幂、阶乘"<<endl<<"TIPS:幂的格式是a^b"<<endl; 
-	int s;
+	int s,p=0;
+	int x=1;
 	double num1=0,num2=0;
+	int fac=0;
 	char n;
 	char arr[1000];  
 	while(1){
@@ -13,6 +15,7 @@ int main(){
 	}
 	num1=0;
 	num2=0;//初始化
+	fac=0;
 	cin>>arr;//输入字符串
 	if(arr[0]=='e'&&arr[1]=='x'&&arr[2]=='i'&&arr[3]=='t'){
 		exit(0);
@@ -39,52 +42,74 @@ int main(){
 			n='^';
 			s=i;
 			break;
+		}else if(arr[i]=='!'){
+			n='!';
+			s=i;
+			break;
 		}
-	}//获取运算符号 
-	int p=0;
-	for(int i=0;i<s;i++){
-		if(arr[i]=='.'){
-			p=s-(i+1);
-		}else{
-			num1=num1*10+(arr[i]-'0');
-		}
+	}//获取运算符号
+	if(n=='!'){
+		p=0;
+		for(int i=0;i<s;i++){
+			if(arr[i]=='.'){
+				p=s-(i+1);
+			}else{
+				fac=fac*10+(arr[i]-'0');
+			}
 		
-	}
-	for(int i=0;i<p;i++){
-		num1=num1/10;
-	}
-	//第一个数 
-	p=0;
-	for(int i=s+1;i<len;i++){
-		if(arr[i]=='.'){
-			p=len-(i+1);
-		}else{
-			num2=num2*10+(arr[i]-'0');
-		}
-	}
-	for(int i=0;i<p;i++){
-		num2=num2/10;
-	}
-	//第2个数
-	if(n=='+'){
-		cout<<num1+num2;
-	}else if(n=='-'){
-		cout<<num1-num2;
-	}else if(n=='*'){
-		cout<<num1*num2;
-	}else if(n=='/'){
-		cout<<num1/num2;
-	}else if(n=='/'){
-		cout<<fixed<<setprecision(10)<<num1/num2;
-	}else if(n=='^'){
-		int x=1;
-		for(int j=0;j<num2;j++){
-			x=x*num1;
+		}//第一个数
+		x=1;
+		for(int i=1;i<=fac;i++){
+				x=x*i;
 		}
 		cout<<x;
+	}else if(n=='+'&&n=='-'&&n=='/'&&n=='*'&&n=='^'){
+		p=0;
+		for(int i=0;i<s;i++){
+			if(arr[i]=='.'){
+				p=s-(i+1);
+			}else{
+				num1=num1*10+(arr[i]-'0');
+			}
+			
+		}
+		for(int i=0;i<p;i++){
+			num1=num1/10;
+		}
+		//第一个数 
+		p=0;
+		for(int i=s+1;i<len;i++){
+			if(arr[i]=='.'){
+				p=len-(i+1);
+			}else{
+				num2=num2*10+(arr[i]-'0');
+			}
+		}
+		for(int i=0;i<p;i++){
+			num2=num2/10;
+		}
+		//第2个数
+		if(n=='+'){
+			cout<<num1+num2;
+		}else if(n=='-'){
+			cout<<num1-num2;
+		}else if(n=='*'){
+			cout<<num1*num2;
+		}else if(n=='/'){
+			cout<<num1/num2;
+		}else if(n=='/'){
+			cout<<fixed<<setprecision(10)<<num1/num2;
+		}else if(n=='^'){
+			x=1;
+			for(int j=0;j<num2;j++){
+				x=x*num1;
+			}
+			cout<<x;
+		}
 	}
+	//算值
 	cout<<endl<<"------------"<<endl;
 	
-	}//算值
+	}
 }
 	
